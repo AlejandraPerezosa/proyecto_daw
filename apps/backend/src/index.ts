c48d78d290614bfa668e1f6a90504fd53e9b6666
Alejandra <alejandrapprim@gmail.com>
2026-04-20 18:05:30 +0200

    conexión bbdd limpio

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,21 +2,22 @@ import dotenv from 'dotenv';
 dotenv.config();
 import {conn} from './config/database';
 
-// Importar express
+// importar express
 import express from 'express';
 const app = express();
 const port = 3001;
 
-// Definir la ruta raíz (/)
+// definir la ruta raíz (/)
 app.get('/', (req, res) => {
-  res.send('¡Hola Mundo!'); // Escribe el texto en la ruta
+  res.send('¡Hola Mundo!'); 
 });
 
-//Iniciar el servidor
+//iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
 
+//comprobar que la conexión funciona
 conn.connect((err) => {
   if (err) {
     console.error('Error conectando a la base de datos:', err);
